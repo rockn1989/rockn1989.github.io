92959fc695538a5a10740db7b9175169252b1bf1
Артём <gold_100@bk.ru>
2016-10-06 15:04:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,4 +2,20 @@
 
 (function() {
 
+  var mainNav = document.querySelector('.main-nav'),
+      openNav = document.querySelector('.main-nav__toggle-menu');
+
+  if(mainNav) {
+    mainNav.classList.remove("main-nav--open");
+    mainNav.classList.remove("main-nav--no-js");
+    mainNav.classList.add("main-nav--close");
+  }
+
+  openNav.addEventListener('click', menuToggle);
+
+  function menuToggle() {
+    mainNav.classList.toggle('main-nav--open');
+    mainNav.classList.toggle('main-nav--close');
+  }
+
 })();
