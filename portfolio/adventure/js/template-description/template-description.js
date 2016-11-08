05f77e696be0e6683c521bc21ad6ca572126efc7
Артём <gold_100@bk.ru>
2016-11-08 21:34:30 +0300

    Add files via upload

--- portfolio/adventure/js/template-description/template-description.js
+++ portfolio/adventure/js/template-description/template-description.js
@@ -4,7 +4,9 @@
 
   var templateBtnOpen = document.querySelector('.template-description__btn-open'),
       templateBtnClose = document.querySelector('.template-description__btn-close'),
-      templateDescription = document.querySelector('.template-description');
+      templateDescription = document.querySelector('.template-description'),
+      templateDescriptionWrapper = document.querySelector('.template-description__wrapper'),
+      templateDescriptionBox = document.querySelector('.template-description__box');
 
   templateBtnOpen.addEventListener('click', showBox);
 
@@ -13,11 +15,15 @@
     templateDescription.style.top = window.pageYOffset + 'px';
     document.body.style.overflow = 'hidden';
     this.classList.add('hide');
+    templateDescriptionWrapper.classList.add('template-animate__wrapper');
     templateBtnClose.addEventListener('click', hideBox);
+    templateDescriptionBox.classList.add('template-animate__box');
   }
 
   function hideBox() {
     templateDescription.classList.remove('open');
+    templateDescriptionWrapper.classList.remove('template-animate__wrapper');
+    templateDescriptionBox.classList.remove('template-animate__box');
     templateBtnOpen.classList.remove('hide');
     document.body.style.overflow = 'auto';
     this.removeEventListener('click', hideBox);
