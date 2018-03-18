f0beabc65c69a69897ea48aa594d56ccefff8663
Артём <gold_100@bk.ru>
2018-03-18 22:23:30 +0300

    Add files via upload

--- portfolio/clinic/js/main.js
+++ portfolio/clinic/js/main.js
@@ -93,6 +93,25 @@ $('.about-company-slider').slick({
 		};
 	}
 
+	/* SERVICE COAST */
+
+	$('.service-coast').on('click','.service-coast__arrow', function() {
+
+		var _self = $(this),
+				parents = $(this).parents('.service-coast'),
+				sublist = $(parents).find('.service-coast__sublist');
+
+		if($(parents).hasClass('open')) {
+			$(sublist).slideUp('350', function() {
+				$(parents).removeClass('open');
+			});
+		} else {
+			$(sublist).slideDown('350', function() {
+				$(parents).addClass('open');
+			});
+		}
+	});
+
 
 	/* YOUTUBE API*/
 
