42de976c319af5c4ead9da0895718e1c7c69128e
Артём <gold_100@bk.ru>
2018-03-25 22:39:32 +0300

    Add files via upload

--- portfolio/clinic/js/main.js
+++ portfolio/clinic/js/main.js
@@ -1,5 +1,111 @@
 'use strict';
 
+/* FIX FOR SELECT2 */
+
+(function($) {
+
+  var Defaults = $.fn.select2.amd.require('select2/defaults');
+
+  $.extend(Defaults.defaults, {
+    dropdownPosition: 'auto'
+  });
+
+  var AttachBody = $.fn.select2.amd.require('select2/dropdown/attachBody');
+
+  var _positionDropdown = AttachBody.prototype._positionDropdown;
+
+  AttachBody.prototype._positionDropdown = function() {
+
+    var $window = $(window);
+
+    var isCurrentlyAbove = this.$dropdown.hasClass('select2-dropdown--above');
+    var isCurrentlyBelow = this.$dropdown.hasClass('select2-dropdown--below');
+
+    var newDirection = null;
+
+    var offset = this.$container.offset();
+
+    offset.bottom = offset.top + this.$container.outerHeight(false);
+
+    var container = {
+        height: this.$container.outerHeight(false)
+    };
+
+    container.top = offset.top;
+    container.bottom = offset.top + container.height;
+
+    var dropdown = {
+      height: this.$dropdown.outerHeight(false)
+    };
+
+    var viewport = {
+      top: $window.scrollTop(),
+      bottom: $window.scrollTop() + $window.height()
+    };
+
+    var enoughRoomAbove = viewport.top < (offset.top - dropdown.height);
+    var enoughRoomBelow = viewport.bottom > (offset.bottom + dropdown.height);
+
+    var css = {
+      left: offset.left,
+      top: container.bottom
+    };
+
+    // Determine what the parent element is to use for calciulating the offset
+    var $offsetParent = this.$dropdownParent;
+
+    // For statically positoned elements, we need to get the element
+    // that is determining the offset
+    if ($offsetParent.css('position') === 'static') {
+      $offsetParent = $offsetParent.offsetParent();
+    }
+
+    var parentOffset = $offsetParent.offset();
+
+    css.top -= parentOffset.top
+    css.left -= parentOffset.left;
+
+    var dropdownPositionOption = this.options.get('dropdownPosition');
+
+    if (dropdownPositionOption === 'above' || dropdownPositionOption === 'below') {
+      newDirection = dropdownPositionOption;
+    } else {
+
+      if (!isCurrentlyAbove && !isCurrentlyBelow) {
+        newDirection = 'below';
+      }
+
+      if (!enoughRoomBelow && enoughRoomAbove && !isCurrentlyAbove) {
+        newDirection = 'above';
+      } else if (!enoughRoomAbove && enoughRoomBelow && isCurrentlyAbove) {
+        newDirection = 'below';
+      }
+
+    }
+
+    if (newDirection == 'above' ||
+    (isCurrentlyAbove && newDirection !== 'below')) {
+        css.top = container.top - parentOffset.top - dropdown.height;
+    }
+
+    if (newDirection != null) {
+      this.$dropdown
+        .removeClass('select2-dropdown--below select2-dropdown--above')
+        .addClass('select2-dropdown--' + newDirection);
+      this.$container
+        .removeClass('select2-container--below select2-container--above')
+        .addClass('select2-container--' + newDirection);
+    }
+
+    this.$dropdownContainer.css(css);
+
+  };
+
+})(window.jQuery);
+
+
+/*_____ SITE CODE _____*/
+
 $(function() {
 
 	/* MAIN SLIDER PRELOAD */
@@ -11,8 +117,8 @@ $(function() {
 	});
 
 
-	/* MAIN SLIDER */
 
+	/* MAIN SLIDER */
 
 	$('.main-slider').slick({
 		arrows: false,
@@ -49,6 +155,7 @@ $(function() {
 			}
 		]
 	});
+
 
 $('.about-company-slider').slick({
 		arrows: false,
@@ -177,10 +284,20 @@ $('.about-company-slider').slick({
 
 	/* CUSTOM SELECT */
 
-	$('.js__custom-select').select2({
-		placeholder: "Выберите специалиста",
-		minimumResultsForSearch: -1
+
+	$('.uk-modal').on('show.uk.modal', function() {
+		var width = $('.default-modal input[type="text"]').outerWidth();
+		$('.js__custom-select').select2({
+			placeholder: "Выберите специалиста",
+			minimumResultsForSearch: -1,
+			dropdownPosition: 'below',
+			width: width+'px'
+		});
 	});
+
+
+
+
 
 
 	/* DATEPICKER EVENTS */
@@ -188,7 +305,9 @@ $('.about-company-slider').slick({
 	var datepicker = UIkit.datepicker($('.custom-input[data-uk-datepicker]'),{offsettop: 0});
 
 	datepicker.on('show.uk.datepicker', function() {
-		$('.custom-input[data-uk-datepicker]').addClass('focused')
+		var width = $('.custom-input[data-uk-datepicker]').outerWidth();
+		$('.uk-dropdown.uk-datepicker').css({'width': width + 'px'});
+		$('.custom-input[data-uk-datepicker]').addClass('focused');
 	})
 	datepicker.on('hide.uk.datepicker', function() {
 		$('.custom-input[data-uk-datepicker]').removeClass('focused')
@@ -282,7 +401,7 @@ $('.about-company-slider').slick({
 		var value = $(this).val().toLowerCase(),
 				valueLength = value.length;
 
-	  if(Date.now() - lastTime >= 800) {
+		if(Date.now() - lastTime >= 800) {
 
 			if(value.length == 0) {
 				servicesArray.map(function(el) {
@@ -296,7 +415,7 @@ $('.about-company-slider').slick({
 				$('.services-list').fadeIn();
 			};
 			lastTime = Date.now();
-	  }
+		}
 
 	});
 
